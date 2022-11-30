f6bc3cd71391d913d2e7a002f846a32de43bc199
FuyaKamata <k021g1486@g.neec.ac.jp>
2022-11-30 12:00:08 +0900

    評価課題01

--- main.cpp
+++ main.cpp
@@ -1,19 +1,52 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-struct LibraryCard {
-	string name;
-	string checkoutDate;
-	string returnDate;
-};
+typedef struct cell
+{
+	int value;
+	struct cell* next;
+}CELL;
+void create(CELL* list, int data)
+{
+	CELL* number;
+	number = (CELL*)malloc(sizeof(CELL));
+	number->value = data;
+	number->next = NULL;
+	while (list->next != NULL)
+	{
+		list = list->next;
+	}
+	list->next = number;
+}
+void index(CELL* list)
+{
 
-int main() {
-	LibraryCard lc;
-	lc.name = "氏 名";
-	lc.checkoutDate = "20221101";
-	lc.returnDate = "20221108";
-	cout << lc.name << " 貸出日は " << lc.checkoutDate;
-	cout << " 返却日は " << lc.returnDate << " です";
+	printf("[");
+	while (list->next != NULL)
+	{
+		list = list->next;
+
+		printf("%d", list->value);
+		printf(",");
+	}
+	printf("]\n");
+}
+int main()
+{
+	int data;
+	CELL head;
+	head.next = nullptr;
+
+	while (true)
+	{
+		printf("好きな数字を入力してください\n");
+		printf("入力した値:");
+		scanf_s("%d", &data);
+		printf("入力された値");
+		create(&head, data);
+
+		index(&head);
+	}
 	return 0;
 }
